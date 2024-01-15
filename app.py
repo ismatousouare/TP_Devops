af01c1c494c5d0f643a4203544e8b1fb7bea30ff
Ismatou <ismatousadio.souare@edu.univ-paris13.fr>
2024-01-16 00:02:18 +0100

    workflow1 update fichier python

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
 
-def addition(a, b):
-    result = a + b
-    return result
+def dire_bonjour():
+    print("Bonjour")
 
-def main():
-    # Entrées pour l'addition
-    num1 = float(input("Entrez le premier nombre : "))
-    num2 = float(input("Entrez le deuxième nombre : "))
-
-    # Appeler la fonction d'addition
-    sum_result = addition(num1, num2)
-
-    # Afficher le résultat
-    print(f"La somme de {num1} et {num2} est : {sum_result}")
 
 if __name__ == "__main__":
     main()
